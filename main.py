d27176227767c4647ac9edbbd6e06ed2b6bad25d
Andrew Berger <bergandr@oregonstate.edu>
2025-08-01 22:55:11 -0700

    Verify server can do basic receive + reply

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # run the microservice
+
+# This code is heavily based on the introduction to ZeroMQ PDF provided on Canvas
+import zmq
+from swap_functions import apply_changes, validate_plan
+
+# establish the context and set up the socket
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+print("Listening for messages ...")
+
+# listen for messages
+try:
+    while True:
+        raw_message = socket.recv_json()
+        if len(raw_message) > 0:
+            socket.send_json(raw_message)
+except KeyboardInterrupt:
+    print("\nShutting down the server ...\n")
+
+# Clear the context and exit
+context.destroy()
